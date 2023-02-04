88eb71ab58dc89b43bdeda1c54d68be1c10e496d
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2023-02-04 18:29:12 +0530

    Lets Revise Concept of arrow function

--- installation-of-Node-Js.js
+++ installation-of-Node-Js.js
@@ -1,1 +1,3 @@
+// Link:- https://drive.google.com/embeddedfolderview?id=1H2qMVChinWAn49mELiEIdnjv2LyoaoO4#list
+
 console.log("Hello World!");
---
+++ let-revise-js-concept.js
@@ -0,0 +1,5 @@
+// Link:- https://drive.google.com/drive/folders/13htSGOoWTZqiZWN9IG7IULOqVE5BW7uN
+
+const product=(a,b)=>a*b;
+console.log(product(10,20))
+
