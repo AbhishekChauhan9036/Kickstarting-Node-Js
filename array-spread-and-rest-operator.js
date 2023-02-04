3a496bb8ad5dc78917bc3a8997d4d72a497523ec
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2023-02-04 18:56:23 +0530

    spread and rest operator

--- array-spread-and-rest-operator.js
+++ array-spread-and-rest-operator.js
@@ -1,31 +1,29 @@
-// Take an array = ['apple', 'oranges' , ' ', 'mango', ' ' , 'lemon]. Transform it into 
+// Take an array = ['apple', 'oranges' , ' ', 'mango', ' ' , 'lemon]. Transform it into
 // ['apple', 'oranges' , 'empty string', 'mango', 'empty string', 'lemon] using array maps
 
-const array = ['apple', 'oranges', ' ', 'mango', ' ', 'lemon'];
+const array = ["apple", "oranges", " ", "mango", " ", "lemon"];
 const ans = array.map((element) => {
-    return element == ' ' ? 'empty string' : element;
-})
-
+  return element == " " ? "empty string" : element;
+});
 console.log(ans);
 
 
 
-
-const obj1 = { 'key1': 1 }
-const obj2 = { ...obj1 }
+const obj1 = { key1: 1 };
+const obj2 = { ...obj1 };
 if (obj2 === obj1) {
-    console.log('same objects')
+  console.log("same objects");
 } else {
-    console.log('different objects')
+  console.log("different objects");
 }
 //Output - different objects
 
 
-const obj3 = { 'key1': 1, 'key2': 2 }
-const obj4 = { ...obj3, key1: 1000 }
-console.log(obj3)
-console.log(obj4)
+
+const obj3 = { key1: 1, key2: 2 };
+const obj4 = { ...obj3, key1: 1000 };
+console.log(obj3);
+console.log(obj4);
 
 //Output- { key1: 1, key2: 2 }
 // { key1: 1000, key2: 2 }
-                         
