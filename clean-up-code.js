a78e96f30b4c125ce12ecf4805dc1eb31487ebbb
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2023-02-05 16:21:00 +0530

    clean up code what is event loop

---
+++ clean-up-code.js
@@ -0,0 +1,4 @@
+/*The event loop is a system used by Node.js to process asynchronous tasks. It continuously 
+monitors for any events that are triggered in the system and queues them up to be executed 
+by the JavaScript engine. This allows for high performance and efficient usage of resources.*/
+
